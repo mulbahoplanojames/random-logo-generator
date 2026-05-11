a7e4a9b2df5244e540ab2523bbed71d25e3ffc1b
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2026-05-11 10:59:07 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,87 @@
+[
+  {
+    name: "Elixir",
+    year: 2012,
+    tag: "functional",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/elixir/elixir-original.svg",
+  },
+  {
+    name: "Scala",
+    year: 2003,
+    tag: "functional",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/scala/scala-original.svg",
+  },
+  {
+    name: "Clojure",
+    year: 2007,
+    tag: "functional",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/clojure/clojure-original.svg",
+  },
+  {
+    name: "Groovy",
+    year: 2003,
+    tag: "scripting",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/groovy/groovy-original.svg",
+  },
+  {
+    name: "Fortran",
+    year: 1957,
+    tag: "scientific",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fortran/fortran-original.svg",
+  },
+  {
+    name: "D",
+    year: 2001,
+    tag: "systems",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/d/d-original.svg",
+  },
+  {
+    name: "OCaml",
+    year: 1996,
+    tag: "functional",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ocaml/ocaml-original.svg",
+  },
+
+  {
+    name: "V",
+    year: 2019,
+    tag: "systems",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/v/v-original.svg",
+  },
+  {
+    name: "Ada",
+    year: 1980,
+    tag: "systems",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ada/ada-original.svg",
+  },
+  {
+    name: "VHDL",
+    year: 1987,
+    tag: "hardware",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vhdl/vhdl-original.svg",
+  },
+  {
+    name: "Common Lisp",
+    year: 1984,
+    tag: "functional",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/commonlisp/commonlisp-original.svg",
+  },
+  {
+    name: "PL/SQL",
+    year: 1991,
+    tag: "database",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg",
+  },
+  {
+    name: "Tcl",
+    year: 1988,
+    tag: "scripting",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tcl/tcl-original.svg",
+  },
+  {
+    name: "Assembly",
+    year: 1949,
+    tag: "systems",
+    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nasm/nasm-original.svg",
+  },
+];
